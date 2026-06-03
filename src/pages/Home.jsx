f810f3cfa49e2ac37413a454318d56c7bd680012
mod0dethink <tanakaryuto1994@gmail.com>
2026-06-03 11:11:12 +0900

    add:pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom'
+
+export function Home() {
+  const navigate = useNavigate()
+
+  return (
+    <div>
+      <h1>
+        ほーむ
+      </h1>
+    </div>
+  )
+}
